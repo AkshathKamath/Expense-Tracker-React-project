873d440c9822091ae26f2f12ee66ae3b969e1b13
Akshath <akshath.kamath2019@vitstudent.ac.in>
2022-06-28 16:38:07 +0530

    Adding bar graph

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,6 +6,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import "./ExpensesList";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -25,6 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpenseChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
